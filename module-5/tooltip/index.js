90c59a660c5c87f5233f831800d15f7bfb63a6bf
aasolovkin <aasolovkin@eossoft.ru>
2020-04-13 22:12:06 +0300

    Реализовал компонент ToolTip

--- module-5/tooltip/index.js
+++ module-5/tooltip/index.js
@@ -9,22 +9,66 @@ class Tooltip {
     }
 
     Tooltip.instance = this;
+
+    this.render();
   }
 
-  initEventListeners () {
+  onPointerover = (event) => {
+    var toolTipedElement = event.target && event.target.closest("[data-tooltip]");
 
+    if (toolTipedElement) {
+      this.element.textContent = toolTipedElement.dataset.tooltip;
+      this.element.style.left = event.clientX + 10 + "px";
+      this.element.style.top = event.clientY + 10 + "px";
+
+      document.body.append(this.element);
+    }
+  };
+
+  onPointerout  = (event) => {
+    if (this.element.parentElement) {
+      this.element.remove();
+    }
+  };
+
+  onPointermove = (event) => {
+    if (this.element.parentElement) {
+      this.element.style.left = event.clientX + 10 + "px";
+      this.element.style.top = event.clientY + 10 + "px";
+    }
+  };
+
+  initEventListeners () {
+    document.body.addEventListener("pointerover", this.onPointerover);
+    document.body.addEventListener("pointerout", this.onPointerout);
+    document.body.addEventListener("pointermove", this.onPointermove);
+  }
+
+  removeEventListeners () {
+    document.body.removeEventListener("pointerover", this.onPointerover);
+    document.body.removeEventListener("pointerout", this.onPointerout);
+    document.body.removeEventListener("pointermove", this.onPointermove);
   }
 
   initialize () {
+    this.initEventListeners();
+  }
 
+  get tooltipTemplate() {
+    return `
+    <div class="tooltip"></div>
+    `;
   }
 
   render () {
-
+    const element = document.createElement('div');
+    element.innerHTML = this.tooltipTemplate;
+    this.element = element.firstElementChild;
   }
 
   destroy () {
-
+    this.removeEventListeners();
+    this.element.remove();
   }
 }
 
