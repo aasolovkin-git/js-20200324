467c508fcd9dca24bbcc721682eeb7f9f81d6947
aasolovkin <aasolovkin@gmail.com>
2020-04-24 14:59:24 +0300

    фывфыв

--- module-8/tasks/double-slider/index.js
+++ module-8/tasks/double-slider/index.js
@@ -29,8 +29,8 @@ export default class DoubleSlider {
       <span data-element="from">${this.formatValue(this.selected.from)}</span>
       <div data-element="inner" class="range-slider__inner">
         <span data-element="progress" class="range-slider__progress" style="left: ${ calcSliderPosition(this.selected.from) + "%" }; right: ${ 100 - calcSliderPosition(this.selected.to) + "%" };"></span>
-        <span data-element="thumbLeft" class="range-slider__thumb-left" style="${ calcSliderPosition(this.selected.from) + "%" };"></span>
-        <span data-element="thumbRight" class="range-slider__thumb-right" style="right: ${ 100 - calcSliderPosition(this.selected.to) + "%" };"></span>
+        <span data-element="thumbLeft" class="range-slider__thumb-left" style="left: ${ calcSliderPosition(this.selected.from) + "%" };"></span>
+        <span data-element="thumbRight" class="range-slider__thumb-right" style="left: ${ calcSliderPosition(this.selected.to) + "%" };"></span>
       </div>
       <span data-element="to">${this.formatValue(this.selected.to)}</span>
     </div>
@@ -68,46 +68,46 @@ export default class DoubleSlider {
       let { left: sliderBarLeft , right: sliderBarRight } = this.subElements.inner.getBoundingClientRect();
       let sliderBarWidth = sliderBarRight - sliderBarLeft;
 
-      let availableDragedSliderRange = isLeftSliderDraged
+      let availableDragedSliderPosition = isLeftSliderDraged
         ? {
             min: 0,
-            max: this.subElements.thumbRight.getBoundingClientRect().left - sliderBarLeft
+            max: parseFloat(this.subElements.thumbRight.style.left)
           }
         : {
-            min: this.subElements.thumbLeft.getBoundingClientRect().right - sliderBarLeft,
-            max: sliderBarWidth
+            min: parseFloat(this.subElements.thumbLeft.style.left),
+            max: 100
           }
 
       let onMouseMove = (event) => {
         const { clientX } = event;
 
         let newLeft = clientX - shiftX - sliderBarLeft;
-    
-        if (newLeft < availableDragedSliderRange.min) {
-          newLeft = availableDragedSliderRange.min;
-        }
-    
-        if (newLeft > availableDragedSliderRange.max) {
-          newLeft = availableDragedSliderRange.max;
-        }
-    
-        let newDragedSliderPositionInPercent = sliderBarWidth 
-          ? newLeft/sliderBarWidth
+
+        let newDragedSliderPosition = sliderBarWidth 
+          ? newLeft/sliderBarWidth * 100
           : isLeftSliderDraged
             ? 0
-            : 1;
+            : 100;
 
-        dragedSlider.style.left = newDragedSliderPositionInPercent * 100 + '%';         
+        if (newDragedSliderPosition < availableDragedSliderPosition.min) {
+          newDragedSliderPosition = availableDragedSliderPosition.min;
+        }
+    
+        if (newDragedSliderPosition > availableDragedSliderPosition.max) {
+          newDragedSliderPosition = availableDragedSliderPosition.max;
+        }
+    
+        dragedSlider.style.left = newDragedSliderPosition + '%';         
 
-        let newDragedSliderValue = Math.round(newDragedSliderPositionInPercent * (this.max - this.min)) + this.min;
+        let newDragedSliderValue = Math.round(newDragedSliderPosition / 100 * (this.max - this.min)) + this.min;
 
         let progressElement = this.subElements.progress;
 
         if (isLeftSliderDraged) {
-          progressElement.style.left = newDragedSliderPositionInPercent * 100 + '%';
+          progressElement.style.left = newDragedSliderPosition + '%';
           this.selected.from = newDragedSliderValue;
         } else {
-          progressElement.style.right = 100 - newDragedSliderPositionInPercent * 100 + '%';
+          progressElement.style.right = 100 - newDragedSliderPosition + '%';
           this.selected.to = newDragedSliderValue;
         }
 
